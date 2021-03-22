83c2f9f63e7f0ad67389c9e00e180ce0c173b3f8
Mariah Morales <mjustine622@gmail.com>
2021-03-22 19:01:03 -0400

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,22 @@
-class Vehicle
+ class Vehicle
 
-end
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size, wheel_number)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end 
+
+    def go
+        return "vrrrrrrrooom!"
+    end 
+
+    def fill_up_tank
+        return "filling up!"
+    end 
+end 
+    
+
+
+
+
